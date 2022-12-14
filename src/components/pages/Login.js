2bc6e88519ccc59d0ab856bed65d5800a6301117
Shivay Bhandari <bhandarishivay2001@gmail.com>
2022-12-14 20:50:53 +0530

    conflict resolved

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -3,7 +3,7 @@ import React from "react";
 const Login = () => {
   return (
     <>
-      <form action="">
+      <form action="/">
         <div>
           <label htmlFor="email">Email</label>
           <input type="email" name="email" id="email" />
